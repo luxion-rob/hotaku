12d311993288e3bf6f404cac6194035ddf01dc0f
BUI-CONG-TRI-BENGO4 <aidaynhi8@gmail.com>
2024-12-20 23:58:15 +0900

    [Feat] Add navbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const value = defineModel<string>({ required: true })
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-between rounded-xl border border-gray-300 px-4 py-2 shadow-inner"
+  >
+    <input
+      class="border-none bg-transparent outline-none"
+      type="text"
+      v-model="value"
+    />
+    <img
+      src="@/assets/icons/search-icon.svg"
+      alt="search-icon"
+      class="h-6 w-6 object-contain"
+    />
+  </div>
+</template>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-import '@/output.css'
-import App from '@/App.vue'
 import router from '@/router'
+import App from '@/App.vue'
 
 const app = createApp(App)
 
