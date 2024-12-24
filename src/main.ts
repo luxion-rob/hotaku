8fb5033d78620ca760c0bac16a4cdd40d1c6d838
BUI-CONG-TRI-BENGO4 <aidaynhi8@gmail.com>
2024-12-24 22:26:37 +0900

    [Setup] Prime vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
 import router from '@/router'
 import App from '@/App.vue'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue)
 router.isReady().then(() => app.mount('#app'))
