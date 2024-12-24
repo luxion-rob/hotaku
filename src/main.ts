1c797bb75545c040b29d8e3bdd91ea1b452ba2c3
BUI-CONG-TRI-BENGO4 <aidaynhi8@gmail.com>
2024-12-24 22:40:49 +0900

    [Setup] Prime vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura';
 import router from '@/router'
 import App from '@/App.vue'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            }
+        }
+    }
+})
 router.isReady().then(() => app.mount('#app'))
