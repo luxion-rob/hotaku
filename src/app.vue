689bb58b63009c3bb3e427fa4529041e9a1d9c8f
luxion-rob <aidaynhi8@gmail.com>
2025-04-03 23:10:52 +0900

    feat: add layout

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  </div>
+  <NuxtLayout name="default">
+    <NuxtApp>
+      <NuxtRouteAnnouncer />
+      <NuxtPage />
+    </NuxtApp>
+  </NuxtLayout>
 </template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const value = ref<string>('')
+
+const input = useTemplateRef('input')
+
+defineShortcuts({
+  '/': () => {
+    input.value?.inputRef?.focus()
+  }
+})
+</script>
+
+<template>
+  <div class="flex col justify-center">
+    <div class="flex justify-center fixed top-0 inset-x-0">
+      <div class="flex justify-between max-w-navbar grow">
+        <div class="flex items-center">
+          <NavbarDrawer class="mr-2"/>
+          <NuxtImg title="logo" src="/logo.png" width="90" class="object-contain"/>
+        </div>
+        <div class="flex justify-end flow items-center">
+          <NuxtInput ref="input" v-model="value" icon="i-lucide-search" highlight title="Search bar" color="primary" variant="soft" placeholder="Search..." class="mr-4">
+            <template #trailing>
+              <NuxtKbd value="/" />
+            </template>
+          </NuxtInput>
+          <NuxtAvatar src="https://github.com/benjamincanac.png" size="xl" />
+        </div>
+      </div>
+    </div>
+    <div class="pt-16 max-w-navbar h-screen overflow-y-auto overflow-x-hidden">
+      <slot />
+    </div>
+  </div>
+</template>
