029f2cc41d0ff97741cb185302f6e5b1a1f4723d
Bui Cong Tri <45875394+luxion-rob@users.noreply.github.com>
2025-04-03 23:33:19 +0900

    Feat: add layout (#17)
    * feat: add layout
    
    * fix: format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,23 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  ssr: false,
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
-
   srcDir: "src",
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image"],
   css: ["assets/global.css"],
+  ui: {
+    prefix: "Nuxt",
+  },
   vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
     plugins: [tailwindcss()],
   },
   nitro: {
---
+++ src/components/navbarDrawer.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtDrawer direction="left">
+    <NuxtButton
+      color="ghost"
+      variant="subtle"
+      icon="i-lucide-menu"
+      :overlay="false"
+    />
+
+    <template #content>
+      <NuxtCollapsible class="flex flex-col gap-2 w-64">
+        <NuxtButton
+          label="Open"
+          color="ghost"
+          variant="subtle"
+          trailing-icon="i-lucide-chevron-down"
+          block
+        />
+        <template #content>
+          <Placeholder class="h-48" />
+        </template>
+      </NuxtCollapsible>
+    </template>
+  </NuxtDrawer>
+</template>
